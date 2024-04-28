e2e236ac89273bf9b9ddee349927a71cea5448bf
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-04-28 14:09:48 +0500

    adding local storage and css

--- src/components/CounterApp.tsx
+++ src/components/CounterApp.tsx
@@ -1,45 +1,58 @@
 import "../styles/dashboard/App.scss";
-import { useState, useEffect } from "react";
-import React from "react";
+import React, { useState, useEffect, useTransition } from "react";
 
 export const CounterApp = () => {
-  const [counts, setCounts] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+  const [isPending, startTransition] = useTransition();
 
   const handleIncreasing = () => {
-    setCounts(counts + 1);
-    localStorage.setItem("numbers", JSON.stringify(counts + 1));
+    startTransition(() => {
+      setCount(count + 5);
+      localStorage.setItem("count", JSON.stringify(count + 5));
+    });
   };
 
   const handleDecreasing = () => {
-    setCounts(counts - 1);
-    localStorage.setItem("numbers", JSON.stringify(counts - 1));
+    startTransition(() => {
+      setCount(count - 5);
+      localStorage.setItem("count", JSON.stringify(count - 5));
+    });
   };
 
   const handleReset = () => {
-    setCounts(0);
-    localStorage.removeItem("numbers");
+    startTransition(() => {
+      setCount(0);
+      localStorage.removeItem("count");
+    });
   };
 
   useEffect(() => {
-    const lol: number = JSON.parse(localStorage.getItem("numbers") || "{}");
-    if (counts) {
-      setCounts(lol);
-    }
-  }, [counts]);
+    const receivingCount = localStorage.getItem("count");
+
+    let processingCount = "";
+
+    receivingCount !== null ? processingCount = JSON.parse(receivingCount) : processingCount = "";
+
+    processingCount ? setCount(parseInt(processingCount)) : setCount(0);
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="header-text">
-          <h1>This Counter App</h1>
-          <h2>Your balance: {counts}</h2>
-        </div>
-        <div className="header-buttons">
-          <button onClick={handleIncreasing}>Increase</button>
-          <button onClick={handleDecreasing}>Decrease</button>
-          <button onClick={handleReset}>Reset</button>
-        </div>
-      </header>
+      <div className="App-text">
+        <h1>Counter</h1>
+        <h2>{count}</h2>
+      </div>
+      <div className="App-buttons">
+        <button onClick={handleIncreasing} disabled={isPending}>
+          +
+        </button>
+        <button onClick={handleDecreasing} disabled={isPending}>
+          -
+        </button>
+        <button onClick={handleReset} disabled={isPending}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
